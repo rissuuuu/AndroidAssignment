4daacd79fc977ba66e897d35bf367d03d9787a90
rissuuuu <rissuuuu@gmail.com>
2020-05-20 10:28:43 +0530

    Assignment

--- app/src/main/java/com/example/assignment/Activity2.java
+++ app/src/main/java/com/example/assignment/Activity2.java
@@ -3,12 +3,19 @@ package com.example.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        textView=findViewById(R.id.viewtext);
+        textView.setText(getIntent().getExtras().getString("key"));
+
+
+
     }
 }
--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText editText;
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.text);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String readtext=editText.getText().toString();
+                if (readtext.equals("")){
+                    Toast.makeText(getApplicationContext(),"Please enter some text",Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    intent.putExtra("key", readtext);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
